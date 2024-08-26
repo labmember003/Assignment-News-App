4a9f7b83bc6bd4107bb084a58c9cef49e46d89c6
Avishisht Gupta <avishishtgupta@gmail.com>
2024-08-27 02:26:21 +0530

    abort html stuff

--- app/src/main/java/com/falcon/assignmentnewsapp/di/NewsRepository.kt
+++ app/src/main/java/com/falcon/assignmentnewsapp/di/NewsRepository.kt
@@ -1,11 +1,14 @@
 package com.falcon.assignmentnewsapp.di
 
+import android.text.Html
 import android.util.Log
 import androidx.core.text.parseAsHtml
 import com.falcon.assignmentnewsapp.modeels.Article
 import com.falcon.assignmentnewsapp.network.DataFromContentService
 import com.falcon.assignmentnewsapp.network.NewsService
 import com.falcon.assignmentnewsapp.room.ArticleDao
+import org.htmlparser.Parser
+import org.htmlparser.nodes.TextNode
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -30,13 +33,27 @@ class NewsRepository @Inject constructor(
             Log.i("NewsRepository", "${response.articles?.size} in db storeed")
         }
     }
+
+//    fun extractTextWithHtmlCleaner(htmlContent: String): String {
+//        val parser = Parser(htmlContent)
+//        val nodeList = parser.parse(null)
+//        return nodeList
+//    }
+    fun extractTextWithStreamApi(htmlContent: String): String {
+        return Html.fromHtml(htmlContent, Html.FROM_HTML_MODE_LEGACY).toString()
+    }
+
     fun fetchWebPageContent(url: String, onResult: (String) -> Unit) {
         val webService = dataFromContentService
         val call = webService.getPageContent(url)
         call.enqueue(object : Callback<String> {
             override fun onResponse(call: Call<String>, response: Response<String>) {
                 if (response.isSuccessful) {
-                    response.body()?.let { onResult(it.parseAsHtml().toString()) }
+                    response.body()?.let {
+                        onResult(extractTextWithStreamApi(it))
+                    }
+//                    Jsoup.parse(htmlContent)
+//                    response.body()
 //                    response.body()?.let { htmlContent ->
 //                        val textContent = parseHtmlToText(htmlContent)
 //                        onResult(textContent)
